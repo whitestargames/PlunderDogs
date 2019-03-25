d73653b6b995642a3cb285898ad02fd63ceb5229
PheroTyrian <trislancaster@gmail.com>
2019-03-25 03:23:11 +0000

    First pass of Map.getMouseClickCoord, added typedef intPair, still need to test

--- HAPI_APP/src/Map.cpp
+++ HAPI_APP/src/Map.cpp
@@ -3,14 +3,18 @@
 #include <math.h>
 #include <algorithm>
 #include <HAPISprites_Lib.h>
-#include <iostream> //For testing
+//#include <iostream> //For testing
 #include "Utilities/Utilities.h"
 
+typedef std::pair<int, int> intPair;
+
 constexpr int FRAME_HEIGHT = 28;
+constexpr int FRAME_CENTRE_X = 15;
+constexpr int FRAME_CENTRE_Y = 34;
 
 void Map::drawMap() const 
 {
-	std::pair<int, int> textureDimensions = std::pair<int, int>(
+	intPair textureDimensions = intPair(
 		m_data[0].m_sprite->FrameWidth(),
 		FRAME_HEIGHT);
 		//m_data[0].m_sprite->FrameHeight());
@@ -47,24 +51,24 @@ void Map::drawMap() const
 	}
 }
 
-std::pair<int, int> Map::offsetToCube(std::pair<int, int> offset) const
+intPair Map::offsetToCube(intPair offset) const
 {
 	int cubeX = offset.first;
 	int cubeY = - offset.first - (offset.second - (offset.first + (offset.first & 1)) / 2);
 	int cubeZ = -cubeX - cubeY;
 	//std::cout << cubeX << ", " << cubeY << ", " << cubeZ << std::endl;//For testing
-	return std::pair<int, int>(cubeX, cubeY);
+	return intPair(cubeX, cubeY);
 }
 
-std::pair<int, int> Map::cubeToOffset(std::pair<int, int> cube) const
+intPair Map::cubeToOffset(intPair cube) const
 {
 	int offsetX = cube.first;
 	int offsetY = -cube.first - cube.second + (cube.first + (cube.first & 1)) / 2;
 	//std::cout << offsetX << ", " << offsetY << std::endl;//For testing
-	return std::pair<int, int>(offsetX, offsetY);
+	return intPair(offsetX, offsetY);
 }
 
-int Map::cubeDistance(std::pair<int, int> a, std::pair<int, int> b) const
+int Map::cubeDistance(intPair a, intPair b) const
 {
 	const int x = abs(a.first - b.first);
 	const int y = abs(a.second - b.second);
@@ -72,9 +76,9 @@ int Map::cubeDistance(std::pair<int, int> a, std::pair<int, int> b) const
 	return std::max(x, std::max(y, z));
 }
 
-bool Map::inCone(std::pair<int, int> orgHex, std::pair<int, int> testHex, eDirection dir) const
+bool Map::inCone(intPair orgHex, intPair testHex, eDirection dir) const
 {
-	const std::pair<int, int> diff(testHex.first - orgHex.first, testHex.second - orgHex.second);
+	const intPair diff(testHex.first - orgHex.first, testHex.second - orgHex.second);
 	const int zDiff = -diff.first - diff.second;
 	if (dir == eNorth || dir == eSouth)//Axis x = 0
 	{
@@ -97,7 +101,19 @@ bool Map::inCone(std::pair<int, int> orgHex, std::pair<int, int> testHex, eDirec
 	return true;
 }
 
-Tile* Map::getTile(std::pair<int, int> coordinate)
+float Map::tileDistanceMag(intPair tileCoord, intPair mouseClick) const
+{
+	std::pair<float, float> tileCentre = getTileScreenPos(tileCoord);
+	tileCentre.first += FRAME_CENTRE_X * m_drawScale;
+	tileCentre.second += FRAME_CENTRE_Y * m_drawScale;
+
+	const float diffX = tileCentre.first - static_cast<float>(mouseClick.first);
+	const float diffY = tileCentre.second - static_cast<float>(mouseClick.second);
+
+	return (diffX * diffX) + (diffY * diffY);
+}
+
+Tile* Map::getTile(intPair coordinate)
 {
 	//Bounds check
 	if (coordinate.first < m_mapDimensions.first &&
@@ -119,32 +135,32 @@ Tile* Map::getTile(std::pair<int, int> coordinate)
 
 //getAdjacentTiles.getSize() == incorrect
 //Refernce Wrapper
-std::vector<Tile*> Map::getAdjacentTiles(std::pair<int, int> coord)
+std::vector<Tile*> Map::getAdjacentTiles(intPair coord)
 {
 	std::vector<Tile*> result;
 	result.reserve(size_t(6));
 	if (coord.first & 1)//Is an odd tile
 	{
-		result.push_back(getTile(std::pair<int, int>(coord.first, coord.second - 1)));		//N
-		result.push_back(getTile(std::pair<int, int>(coord.first + 1, coord.second - 1)));	//NE
-		result.push_back(getTile(std::pair<int, int>(coord.first + 1, coord.second)));		//SE
-		result.push_back(getTile(std::pair<int, int>(coord.first, coord.second + 1)));		//S
-		result.push_back(getTile(std::pair<int, int>(coord.first - 1, coord.second)));		//SW
-		result.push_back(getTile(std::pair<int, int>(coord.first - 1, coord.second - 1)));	//NW
+		result.push_back(getTile(intPair(coord.first, coord.second - 1)));		//N
+		result.push_back(getTile(intPair(coord.first + 1, coord.second - 1)));	//NE
+		result.push_back(getTile(intPair(coord.first + 1, coord.second)));		//SE
+		result.push_back(getTile(intPair(coord.first, coord.second + 1)));		//S
+		result.push_back(getTile(intPair(coord.first - 1, coord.second)));		//SW
+		result.push_back(getTile(intPair(coord.first - 1, coord.second - 1)));	//NW
 	}
 	else//Is even
 	{
-		result.push_back(getTile(std::pair<int, int>(coord.first, coord.second - 1)));		//N
-		result.push_back(getTile(std::pair<int, int>(coord.first + 1, coord.second)));		//NE
-		result.push_back(getTile(std::pair<int, int>(coord.first + 1, coord.second + 1)));	//SE
-		result.push_back(getTile(std::pair<int, int>(coord.first, coord.second + 1)));		//S
-		result.push_back(getTile(std::pair<int, int>(coord.first - 1, coord.second + 1)));	//SW
-		result.push_back(getTile(std::pair<int, int>(coord.first - 1, coord.second)));		//NW
+		result.push_back(getTile(intPair(coord.first, coord.second - 1)));		//N
+		result.push_back(getTile(intPair(coord.first + 1, coord.second)));		//NE
+		result.push_back(getTile(intPair(coord.first + 1, coord.second + 1)));	//SE
+		result.push_back(getTile(intPair(coord.first, coord.second + 1)));		//S
+		result.push_back(getTile(intPair(coord.first - 1, coord.second + 1)));	//SW
+		result.push_back(getTile(intPair(coord.first - 1, coord.second)));		//NW
 	}
 	return result;
 }
 
-std::vector<Tile*> Map::getTileRadius(std::pair<int, int> coord, int range)
+std::vector<Tile*> Map::getTileRadius(intPair coord, int range)
 {
 	if (range < 1)
 		HAPI_Sprites.UserMessage("getTileRadius range less than 1", "Map error");
@@ -158,7 +174,7 @@ std::vector<Tile*> Map::getTileRadius(std::pair<int, int> coord, int range)
 
 	tileStore.reserve((size_t)reserveSize);
 
-	std::pair<int, int> cubeCoord(offsetToCube(coord));
+	intPair cubeCoord(offsetToCube(coord));
 
 	for (int y = std::max(0, coord.second - range);
 		y < std::min(m_mapDimensions.second, coord.second + range + 1);
@@ -170,9 +186,9 @@ std::vector<Tile*> Map::getTileRadius(std::pair<int, int> coord, int range)
 		{
 			if (!(coord.first == x && coord.second == y))//If not the tile at the centre
 			{
-				if (cubeDistance(cubeCoord, offsetToCube(std::pair<int, int>(x, y))) <= range)
+				if (cubeDistance(cubeCoord, offsetToCube(intPair(x, y))) <= range)
 				{
-					tileStore.push_back(getTile(std::pair<int, int>(x, y)));
+					tileStore.push_back(getTile(intPair(x, y)));
 					//std::cout << "CubeCheck " << x << ", " << y << std::endl;//For testing
 				}
 			}
@@ -181,7 +197,7 @@ std::vector<Tile*> Map::getTileRadius(std::pair<int, int> coord, int range)
 	return tileStore;
 }
 
-std::vector<Tile*> Map::getTileCone(std::pair<int, int> coord, int range, eDirection direction)
+std::vector<Tile*> Map::getTileCone(intPair coord, int range, eDirection direction)
 {
 	if (range < 1)
 		HAPI_Sprites.UserMessage("getTileCone range less than 1", "Map error");
@@ -194,7 +210,7 @@ std::vector<Tile*> Map::getTileCone(std::pair<int, int> coord, int range, eDirec
 	std::vector<Tile*> tileStore;
 	tileStore.reserve((size_t)reserveSize);
 
-	const std::pair<int, int> cubeCoord(offsetToCube(coord));
+	const intPair cubeCoord(offsetToCube(coord));
 
 	for (int y = std::max(0, coord.second - range - 1);
 		y < std::min(m_mapDimensions.second, coord.second + range + 2);
@@ -206,12 +222,12 @@ std::vector<Tile*> Map::getTileCone(std::pair<int, int> coord, int range, eDirec
 		{
 			if (!(coord.first == x && coord.second == y))//If not the tile at the centre
 			{
-				std::pair<int, int> tempCube(offsetToCube(std::pair<int, int>(x, y)));
+				intPair tempCube(offsetToCube(intPair(x, y)));
 				if (cubeDistance(cubeCoord, tempCube) <= range)
 				{
 					if (inCone(cubeCoord, tempCube, direction))
 					{
-						tileStore.push_back(getTile(std::pair<int, int>(x, y)));
+						tileStore.push_back(getTile(intPair(x, y)));
 						//std::cout << "CubeCheck " << x << ", " << y << std::endl;//For testing
 					}
 				}
@@ -221,7 +237,7 @@ std::vector<Tile*> Map::getTileCone(std::pair<int, int> coord, int range, eDirec
 	return tileStore;
 }
 
-bool Map::moveEntity(std::pair<int, int> originalPos, std::pair<int, int> newPos)
+bool Map::moveEntity(intPair originalPos, intPair newPos)
 {
 	if (!getTile(newPos) || !getTile(originalPos))
 		return false;
@@ -236,7 +252,7 @@ bool Map::moveEntity(std::pair<int, int> originalPos, std::pair<int, int> newPos
 	return true;
 }
 
-void Map::insertEntity(Entity& newEntity, std::pair<int, int> coord)
+void Map::insertEntity(Entity& newEntity, intPair coord)
 {
 	Tile* tile = getTile(coord);	
 	if (tile && !tile->m_entityOnTile)
@@ -245,9 +261,9 @@ void Map::insertEntity(Entity& newEntity, std::pair<int, int> coord)
 	}
 }
 
-std::pair<int, int> Map::getTileScreenPos(std::pair<int, int> coord) const
+intPair Map::getTileScreenPos(intPair coord) const
 {
-	std::pair<int, int> textureDimensions = std::pair<int, int>(
+	intPair textureDimensions = intPair(
 		m_data[0].m_sprite->FrameWidth(),
 		FRAME_HEIGHT);
 
@@ -255,15 +271,46 @@ std::pair<int, int> Map::getTileScreenPos(std::pair<int, int> coord) const
 	const float yPos = (float)((((1 + coord.first) % 2) + 2 * coord.second) 
 		* textureDimensions.second) / 2;
 
-	return std::pair<int, int>(
+	return intPair(
 		xPos * m_drawScale - m_drawOffset.first,
 		yPos * m_drawScale - m_drawOffset.second);
 }
 
-Map::Map(std::pair<int, int> size, const std::vector<std::vector<int>>& tileData) :
+intPair Map::getMouseClickCoord(intPair mouseCoord) const
+{
+	intPair textureDimensions = intPair(
+		m_data[0].m_sprite->FrameWidth(),
+		FRAME_HEIGHT);
+	
+	const float translatedX = (mouseCoord.first + m_drawOffset.first - FRAME_CENTRE_X) / m_drawScale;
+	const float translatedY = (mouseCoord.second + m_drawOffset.second - FRAME_CENTRE_Y) / m_drawScale;
+	const int predictedTileX = static_cast<const int>(translatedX * 4 / (3 * textureDimensions.first));
+	const int predictedTileY = static_cast<const int>(translatedY / textureDimensions.second);
+	
+	std::cout << "Tile guess: " << predictedTileX << ", " << predictedTileY;//Test
+	float distance{ 10 };
+	intPair closestTile(predictedTileX - 1, predictedTileY - 1);
+	//Iterate through the 9 tiles around the guess to find the actual closest tile to the click
+	for (int y = predictedTileY - 1; y <= predictedTileY + 1; y++)
+	{
+		for (int x = predictedTileX - 1; x <= predictedTileX + 1; x++)
+		{
+			const float tempDist = tileDistanceMag(intPair(x, y), mouseCoord);
+			if (tempDist < distance)
+			{
+				distance = tempDist;
+				closestTile = intPair(x, y);
+			}
+		}
+	}
+	std::cout << " Final tile: " << closestTile.first << ", " << closestTile.second << std::endl;//Test
+	return closestTile;
+}
+
+Map::Map(intPair size, const std::vector<std::vector<int>>& tileData) :
 	m_mapDimensions(size),
 	m_data(),
-	m_drawOffset(std::pair<int, int>(10, 60)),
+	m_drawOffset(intPair(10, 60)),
 	m_windDirection(eNorth),
 	m_windStrength(0.0),
 	m_drawScale(2),
@@ -284,7 +331,7 @@ Map::Map(std::pair<int, int> size, const std::vector<std::vector<int>>& tileData
 			const int tileID = tileData[y][x];
 			assert(tileID != -1);
 			m_data.emplace_back(static_cast<eTileType>(tileID), 
-				motherSprite->GetSpritesheet(), std::pair<int, int>(x, y));
+				motherSprite->GetSpritesheet(), intPair(x, y));
 
 			if (!m_data[x + y * m_mapDimensions.first].m_sprite)
 			{
@@ -292,7 +339,7 @@ Map::Map(std::pair<int, int> size, const std::vector<std::vector<int>>& tileData
 				return;
 			}
 			m_data[x + y * m_mapDimensions.first].m_sprite->SetFrameNumber(tileID);
-			//cubeToOffset(offsetToCube(std::pair<int, int>(x, y)));
+			//cubeToOffset(offsetToCube(intPair(x, y)));
 		}
 	}
 }
