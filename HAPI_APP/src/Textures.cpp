52f1c6129006135f6303b53b76c69ecb0f8f4ca7
Ryan Swann <RyanSwann1992@gmail.com>
2019-04-07 01:25:21 +0100

    Implemented Texture Holder.

---
+++ HAPI_APP/src/Textures.cpp
@@ -0,0 +1,10 @@
+#include "Textures.h"
+
+std::shared_ptr<SpriteSheet> Textures::m_mouseCrossHair;
+std::shared_ptr<SpriteSheet> Textures::m_battleMapBackground;
+std::shared_ptr<SpriteSheet> Textures::m_enemyTerritoryHexSheet;
+std::shared_ptr<SpriteSheet> Textures::m_prebattleUIBackground;
+std::shared_ptr<SpriteSheet> Textures::m_preBattleUIPlayButton;
+std::shared_ptr<SpriteSheet> Textures::m_preBattleUIBackButton;
+std::shared_ptr<SpriteSheet> Textures::m_thing;
+std::shared_ptr<SpriteSheet> Textures::m_hexTiles;
