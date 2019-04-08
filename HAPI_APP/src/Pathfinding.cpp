9566d03bd868ae77c16a7d5a4a3106a02bd239b0
Ryan Swann <RyanSwann1992@gmail.com>
2019-04-08 15:49:19 +0100

    Pathfinding Fix for Detecting Entities.

--- HAPI_APP/src/Pathfinding.cpp
+++ HAPI_APP/src/Pathfinding.cpp
@@ -68,8 +68,8 @@ bool isUnBlocked(const Map & map, std::pair<int, int> coord)
 {
 	auto * tile = map.getTile(coord);
 	if (tile != nullptr)
-		if (tile->m_type == eTileType::eSea ||
-			tile->m_type == eTileType::eOcean &&
+		if ((tile->m_type == eTileType::eSea ||
+			tile->m_type == eTileType::eOcean) &&
 			map.getTile(coord)->m_entityOnTile == nullptr )
 			return true;
 	return false;
