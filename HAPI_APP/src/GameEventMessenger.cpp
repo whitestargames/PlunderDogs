0e8b8aae57acda2ae17567ae31c5288f721f2a25
SWANN-BOWLES <T7009795@live.tees.ac.uk>
2019-04-23 23:48:37 +0100

    Removed comment.

--- HAPI_APP/src/GameEventMessenger.cpp
+++ HAPI_APP/src/GameEventMessenger.cpp
@@ -1,5 +1,3 @@
 #include "GameEventMessenger.h"
 
 std::unordered_map<GameEvent, std::vector<Listener>>  GameEventMessenger::m_listeners;
-
-//OverWorldWindow OverWorldGUI::CURRENT_WINDOW = OverWorldWindow::eLevelSelection;
