e9a2e4add07c5d0534aefb10d253f38556ed8c3a
PheroTyrian <trislancaster@gmail.com>
2019-04-27 21:04:56 +0100

    Created findClosestEnemy definition

--- HAPI_APP/AI.cpp
+++ HAPI_APP/AI.cpp
@@ -2,20 +2,32 @@
 
 constexpr int MAX_INT{ 2147483647 };
 
-Tile* AI::findClosestEnemy(
+const Tile* AI::findClosestEnemy(
 	const Battle* battlePtr, const Map* mapPtr, const BattleEntity* alliedShip, FactionName faction) const
 {
 	Tile* closestEnemy{ nullptr };
 	int closestDistance{ MAX_INT };
+	std::pair<int, int> alliedPos{ 
+		mapPtr->getTileScreenPos(alliedShip->m_battleProperties.getCurrentPosition()) };
 	for (int i = 0; i < 4; i++)
 	{
 		if (i == static_cast<int>(faction))
 			continue;
-		auto factionShipList = battlePtr->getFactionShips(static_cast<FactionName>(i));
-		for (auto it : *factionShipList)
+		const auto factionShipList = *battlePtr->getFactionShips(static_cast<FactionName>(i));
+		for (int j = 0; j < factionShipList.size(); j++)
 		{
-			std::pair<int, int> enemyPos = it.m_battleProperties.getCurrentPosition();
-			if ()
+			//Find the distance^2 from the allied ship to the enemy ship, 
+			//then set closestEnemy to that enemy if it's the closest yet found
+			std::pair<int, int> enemyPos = mapPtr->getTileScreenPos(
+				factionShipList[j]->m_battleProperties.getCurrentPosition());
+			std::pair<int, int> diff(
+				{ enemyPos.first - alliedPos.first, enemyPos.second - alliedPos.second });
+			int enemyDistance = diff.first * diff.first + diff.second * diff.second;
+			if (enemyDistance < closestDistance)
+			{
+				closestDistance = enemyDistance;
+				closestEnemy = factionShipList[j]->m_battleProperties.getCurrentPosition;
+			}
 		}
 	}
 	return closestEnemy;
@@ -23,7 +35,7 @@ Tile* AI::findClosestEnemy(
 
 void AI::handleMovementPhase(Battle* battlePtr, Map* mapPtr, FactionName faction)
 {
-	std::vector<BattleEntity>* ships{ battlePtr->getFactionShips(faction) };
+	std::vector<BattleEntity>* ships{ battlePtr->getFactionShips() };
 	//loop through all the ships in the faction
 	for (auto it = ships->begin(); it != ships->end(); it++)
 	{
--- HAPI_APP/AI.h
+++ HAPI_APP/AI.h
@@ -8,7 +8,7 @@
 class AI
 {
 private:
-	Tile* findClosestEnemy(const Battle* battlePtr, const Map* mapPtr, const Tile* alliedShip, FactionName faction) const;
+	const Tile* findClosestEnemy(const Battle* battlePtr, const Map* mapPtr, const BattleEntity* alliedShip, FactionName faction) const;
 	std::pair<Tile*,eDirection> findFiringPosition(const Tile* targetShip, eWeaponType weapon, int range) const;
 	void attemptShot(const Tile* firingShip, const Tile* targetShip);
 	void attemptMove(Tile* currentTile, std::pair<Tile*, eDirection> targetTile);
