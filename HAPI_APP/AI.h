9a45e364e03d81a105cee5c6ff9206b688cbe00d
Ink <47125846+RKilpatrick@users.noreply.github.com>
2019-04-27 21:20:05 +0100

    Update AI.h

--- HAPI_APP/AI.h
+++ HAPI_APP/AI.h
@@ -8,8 +8,8 @@
 namespace AI
 {
 //Internal use
-	const Tile* findClosestEnemy(const Battle* battlePtr, const Map* mapPtr, const BattleEntity* alliedShip, FactionName faction) const;
-	std::pair<Tile*,eDirection> findFiringPosition(const Tile* targetShip, eWeaponType weapon, int range) const;
+	const Tile* findClosestEnemy(const Battle* battlePtr, const Map* mapPtr, const std::unique_ptr<BattleEntity> alliedShip, FactionName faction);
+	std::pair<Tile*,eDirection> findFiringPosition(const Tile* targetShip, eWeaponType weapon, int range);
 	void attemptShot(const Tile* firingShip, const Tile* targetShip);
 	void attemptMove(Tile* currentTile, std::pair<Tile*, eDirection> targetTile);
 //External use
