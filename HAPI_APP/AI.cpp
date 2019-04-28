0824ca0340b4d1b9cb0e51b95dbf9d64b8d1ecda
RKilpatrick <47125846+RKilpatrick@users.noreply.github.com>
2019-04-28 19:18:48 +0100

    AIing

--- HAPI_APP/AI.cpp
+++ HAPI_APP/AI.cpp
@@ -36,7 +36,7 @@ const Tile* AI::findClosestEnemy(
 	}
 	return closestEnemy;
 }
-//Support function for AI::findFiringPositions
+//Support function for AI::findFiringPositions. Finds the closest tile a specified distance away from a target tile
 const Tile* firePosRadial(
 	Map* mapPtr, const Tile* targetShip, const Tile* alliedShip, int range)
 {
@@ -104,19 +104,19 @@ void AI::handleMovementPhase(Battle* battlePtr, Map* mapPtr, FactionName faction
 		const Tile* enemyPosition{ findClosestEnemy(battlePtr, mapPtr, ships[i], faction) };
 
 		//find the nearest tile and facing that can fire upon the chosen enemy ship
-		std::pair<Tile*, eDirection>  firingPosition{ findFiringPosition(enemyPosition,ships[i]->m_entityProperties.m_weaponType, ships[i]->m_entityProperties.m_range) };
+		std::pair<const Tile*, eDirection>  firingPosition{ findFiringPosition(mapPtr, enemyPosition, mapPtr->getTile(ships[i]->m_battleProperties.getCurrentPosition()), ships[i]->m_entityProperties.m_weaponType, ships[i]->m_entityProperties.m_range) };
 		//move as far as possible on the path to the chosen position
-		attemptMove(mapPtr->getTile(ships[i]->m_battleProperties.getCurrentPosition()), firingPosition);
+		attemptMove(ships[i], firingPosition);
 	}
 }
 
 void AI::handleShootingPhase(Battle* battlePtr, Map* mapPtr, FactionName faction)
-{  
-	std::vector<BattleEntity> ships{ battlePtr->getFactionShips(faction) };
+{
+	auto ships = *battlePtr->getFactionShips(faction);
 	//loop through all the ships in the faction
-	for (auto it = ships.begin(); it != ships.end(); it++)
+	for (int i = 0; i < ships.size(); i++)
 	{
 		//check if the ship is able to fire upon any enemies and fire if possible
-		attemptShot(mapPtr->getTile(it->m_battleProperties.getCurrentPosition()));
+		attemptShot(ships[i]);
 	}
 }
